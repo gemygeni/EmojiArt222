5b2fd906cbb33d2312f0bae3490264f4bc11eb3c
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2019-12-07 02:54:36 +0200

    Initial Commit

---
+++ EmojiArt222/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt222
+//
+//  Created by AHMED GAMAL  on 12/7/19.
+//  Copyright © 2019 AHMED GAMAL . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
